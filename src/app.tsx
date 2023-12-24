73a4a9d039afc7ae46a9fc1a83c9748d064f9b66
zzompang2 <zzompang2@gmail.com>
2023-12-24 21:09:41 +0900

    add/delete member

--- src/app.tsx
+++ src/app.tsx
@@ -3,20 +3,42 @@ import { createSignal } from "solid-js";
 import "./app.css";
 
 export default function App() {
-  const [count, setCount] = createSignal(0);
+  const [members, setMembers] = createSignal(Array<any>());
+  let inputRef: HTMLInputElement;
+
+  const handleAddMember = (e: Event) => {
+    e.preventDefault(); // prevent refresh
+
+    const trimedName = inputRef.value.trim();
+    inputRef.value = "";
+
+    if (trimedName === "") return;
+    setMembers([...members(), trimedName]);
+  };
+
+  const handleDeleteMember = (index: number) => {
+    console.log(index);
+    setMembers(members().filter((_, i) => i !== index));
+  };
+
   return (
     <main>
-      <h1>Hello world!</h1>
-      <button class="increment" onClick={() => setCount(count() + 1)}>
-        Clicks: {count()}
-      </button>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+      <div>{members().length}명</div>
+      <form onSubmit={handleAddMember}>
+        <input type="text" ref={(ref) => (inputRef = ref)} placeholder="이름" />
+        <button type="submit">추가</button>
+      </form>
+      <p>엔터키로 빠르게 추가할 수 있어요!</p>
+      <div>
+        {members().map((mem, i) => (
+          <div>
+            <span>
+              {i + 1}. {mem}
+            </span>
+            <button onclick={() => handleDeleteMember(i)}>삭제</button>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
