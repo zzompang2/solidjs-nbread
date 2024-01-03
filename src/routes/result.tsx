a2db00334f36be6fbfedd2fb5a775143b46bd1e3
zzompang2 <zzompang2@gmail.com>
2024-01-03 10:51:31 +0900

    copy to clipboard

--- src/routes/result.tsx
+++ src/routes/result.tsx
@@ -4,6 +4,7 @@ import { createSignal, For } from "solid-js";
 import PayInfoItem from "~/components/payInfoItem";
 import SendingItem from "~/components/sendingItem";
 import { calculateNBread } from "~/systems/calculate";
+import { memberList } from "~/systems/data";
 import { setTab, TabMenu } from "~/systems/signal";
 import { TEXT } from "~/systems/text";
 
@@ -12,6 +13,22 @@ export default function Result() {
   const [payInfoList, sendingList] = calculateNBread();
   console.log("payInfoList", payInfoList);
   console.log("sendingList", sendingList);
+
+  const copyResultText = async () => {
+    let result = sendingList.reduce((pre, sending) => {
+      const sender = memberList.name(sending.from);
+      const taker = memberList.name(sending.to);
+      return pre + `${sender} --> ${taker} : ${sending.money}원\n`;
+    }, "[N빵 정산 결과]\n\n");
+
+    result += "\n(똑똑하게 정산하자! N-bread)";
+    try {
+      await navigator.clipboard.writeText(result);
+      alert("클립보드에 링크가 복사되었습니다.");
+    } catch (e) {
+      alert("복사에 실패하였습니다");
+    }
+  };
 
   return (
     <div class="ctn" onclick={() => selectInfo(0)}>
@@ -36,8 +53,9 @@ export default function Result() {
                 {(sending) => <SendingItem sending={sending} />}
               </For>
             </div>
+            <button onclick={() => copyResultText()}>텍스트로 복사하기</button>
           </div>
-          <div class="middle_title">디테일한 정보 보기</div>
+          <div class="middle_title">디테일한 정보</div>
           <div class="table">
             <span class="col_flexible">{TEXT.result.label4}</span>
             <span class="col_money">{TEXT.result.label5}</span>
